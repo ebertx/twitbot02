499fb0831670d0a9a84a476f54206e1dec500d51
Christian <ebertx@gmail.com>
2013-08-07 18:08:19 -0600

    removed commented code, changed duration

--- app.js
+++ app.js
@@ -3,26 +3,6 @@ var config = require('./node_modules/twit/config.js');
 
 var bot = new Bot(config);
 
-// var Twit = require('twit');
-// var twit = new Twit(config);
-
-/*
-var Bot = module.exports = function(config) { 
-  this.twit = new Twit(config);
-};
-
-console.log(Bot);
-var bot = new Bot(config);
-
-*/
-
-/*
-var Bot = require('./bot')
-  , config = require('../config');
-
-var bot = new Bot(config);
-*/
-
 console.log('twitbot2: Running.');
 
 //get date string for today's date (e.g. '2011-01-01')
@@ -41,40 +21,6 @@ setInterval(function() {
   var rand = Math.random();
   
   if(rand <= .30) {      
-    //  tweet popular github tweet
-    /*
-    var params = {
-        q: 'github.com/'
-      , since: datestring()
-      , result_type: 'mixed'
-    };
-    bot.twit.get('search', params, function (err, reply) {
-      if(err) return handleError(err);
-      
-      var max = 0, popular;
-      
-      var tweets = reply.results
-        , i = tweets.length;
-      
-      while(i--) {
-        var tweet = tweets[i]
-          , popularity = tweet.metadata.recent_retweets;
-          
-        if(popularity > max) {
-          max = popularity;
-          popular = tweet.text;
-        }
-      }
-      
-      bot.tweet(popular, function (err, reply) {
-        if(err) return handleError(err);
-
-        console.log('\nTweet: ' + (reply ? reply.text : reply));
-      })
-
-    });
-    */
-
     // favorite popular hamster tweets
     var params = {
         q: 'hamster'
@@ -138,7 +84,7 @@ setInterval(function() {
       console.log('\nPrune: unfollowed @'+ name);
     });
   }
-}, 1000);
+}, 10000);
 
 function handleError(err) {
   console.error('response status:', err.statusCode);
